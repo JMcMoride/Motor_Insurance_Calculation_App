1ec05bc7d7398a4e373aa55f1f6998994e961f7b
Josh McM <jrmcm.42@gmail.com>
2017-09-05 15:35:39 +0100

    Add project files.

---
+++ Motor_Insurance_Calculation_App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motor_Insurance_Calculation_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
